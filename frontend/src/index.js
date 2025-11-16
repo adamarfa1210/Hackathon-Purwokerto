1c77a805fe1c1b7bf9d3e7a842c0b13eff30f880
adamarfa1210 <adamarfa121006@gmail.com>
2025-11-16 19:44:58 +0700

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dashboard from './Dashboard.jsx'; // Mengimpor komponen utama
+import Dashboard from './Dashboard.jsx';
 
+// --- TAMBAHKAN KODE INI UNTUK MEMBATALKAN PENDAFTARAN SERVICE WORKER ---
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then(registration => {
+    registration.unregister();
+  });
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
